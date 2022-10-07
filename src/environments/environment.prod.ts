35fc0ad81a49f7605eae59b30ef0105a1c89e398
luisaromero <luisa.marcela.romero@gmail.com>
2022-10-07 15:32:56 -0300

    add firebase to the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyC_vDDGreRngOFbYw_nNceNHzZQlKiOrtQ",
+    authDomain: "accesible-maps-angular.firebaseapp.com",
+    projectId: "accesible-maps-angular",
+    storageBucket: "accesible-maps-angular.appspot.com",
+    messagingSenderId: "596128002351",
+    appId: "1:596128002351:web:236331b4f63d245acdf1da"
+  }
 };
